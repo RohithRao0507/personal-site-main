f9acceaf58b835011865ce437d5302c5895f3126
Rohith <rohithvujjini0507@gmail.com>
2024-11-27 10:46:04 -0600

    raw setup

---
+++ src/components/Template/SideBar.js
@@ -0,0 +1,54 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+import ContactIcons from '../Contact/ContactIcons';
+
+const { PUBLIC_URL } = process.env; // set automatically from package.json:homepage
+
+const SideBar = () => (
+  <section id="sidebar">
+    <section id="intro">
+      <Link to="/" className="logo">
+        <img src={`${PUBLIC_URL}/images/me.jpg`} alt="" />
+      </Link>
+      <header>
+        <h2>Rohith Rao Vujjini</h2>
+      </header>
+      <ContactIcons />
+    </section>
+
+    <section className="blurb">
+      <h2>About</h2>
+      <p>
+        Hi, I&apos;m Rohith. I am a{' '}
+        <a href="https://uiowa.edu/">University of Iowa</a> grad student in cs, YC
+        alumnus, and the co-founder and CTO of{' '}
+        <a href="https://promptfoo.dev">Promptfoo</a>. Previously, I
+        was VP of Engineering at <a href="https://usesmileid.com">SmileID</a>,
+        co-founder and CTO of <a href="https://arthena.com">Arthena</a>, and
+        co-founded <a href="https://matroid.com">Matroid</a>.
+      </p>
+      <ul className="actions">
+        <li>
+          {!window.location.pathname.includes('/resume') ? (
+            <Link to="/resume" className="button">
+              Learn More
+            </Link>
+          ) : (
+            <Link to="/about" className="button">
+              About Me
+            </Link>
+          )}
+        </li>
+      </ul>
+    </section>
+
+    <section id="footer">
+      <p className="copyright">
+        &copy; Rohith Rao Vujjini <Link to="/">mldangelo.com</Link>.
+      </p>
+    </section>
+  </section>
+);
+
+export default SideBar;
---
+++ src/data/resume/degrees.js
@@ -0,0 +1,16 @@
+const degrees = [
+  {
+    school: 'University of Iowa',
+    degree: 'Masters of Computer Science',
+    link: 'https://cs.uiowa.edu/graduate/mcs-computer-science',
+    year: 2025,
+  },
+  {
+    school: 'NIIT University',
+    degree: 'B.Tech Computer Science, Minor Data Science',
+    link: '',
+    year: 2023,
+  },
+];
+
+export default degrees;
